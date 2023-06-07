c88082aa0ab11e41f6e2d752eb2b5363e9fef276
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2023-06-07 23:17:24 +0530

    Minor Changes
    Add UserAccounts Model

---
+++ Shoppinz/RecommendationServices/Model/UserAccounts.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RecommendationServices.Model
+{
+    public class UserAccounts
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? ID { get; set; }
+
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string[] PersonalCard { get; set; } = null!; //First Name , NIC , DOB, Last Name and Other Personal Details
+
+        public string PersonalCode { get; set; } = null!; //Application predefined code for Preferences
+    }
+}
